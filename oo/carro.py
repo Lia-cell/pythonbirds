750a3c7de5bcab2978128627fdca5ec17924677c
santa <santana_lia@hotmail.com>
2021-07-01 21:35:19 -0300

    implementada direcao

--- oo/carro.py
+++ oo/carro.py
@@ -42,34 +42,34 @@ O   L
      >>> motor.velocidade
      0
      >>> # Testando Direcão
-     >>> direcao = direcao()
+     >>> direcao = Direcao()
      >>> direcao.valor
      'Norte'
-     >>> direcao.girar_a_direita
+     >>> direcao.girar_a_direita()
      >>> direcao.valor
      'Leste'
-     >>> direcao.girar_a_direita
+     >>> direcao.girar_a_direita()
      >>> direcao.valor
      'Sul'
-     >>> direcao.girar_a_direita
+     >>> direcao.girar_a_direita()
      >>> direcao.valor
      'Oeste'
-     >>> direcao.girar_a_direita
+     >>> direcao.girar_a_direita()
      >>> direcao.valor
      'Norte'
-     >>> direcao.girar_a_esquerda
+     >>> direcao.girar_a_esquerda()
      >>> direcao.valor
-     'Oeste
-     >>> direcao.girar_a_esquerda
+     'Oeste'
+     >>> direcao.girar_a_esquerda()
      >>> direcao.valor
      'Sul'
-     >>> direcao.girar_a_esquerda
+     >>> direcao.girar_a_esquerda()
      >>> direcao.valor
      'Leste'
-     >>> direcao.girar_a_esquerda
+     >>> direcao.girar_a_esquerda()
      >>> direcao.valor
      'Norte'
-     >>> carro = carro(direcao, motor)
+     >>> carro = Carro(direcao, Motor)
      >>> carro.calcular_velocidade()
      0
      >>> carro.acelerar()
@@ -104,18 +104,18 @@ class Direcao:
     rotacao_a_direita_dct = {
         NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
     }
-    rotacao_a_esquerda_dct = {
+    rotacao_a_esquerda_dct ={
         NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
     }
 
     def __init__(self):
-        self.valor = NORTE
+      self.valor = NORTE
 
     def girar_a_direita(self):
         self.valor = self.rotacao_a_direita_dct[self.valor]
+
     def girar_a_esquerda(self) :
             self.valor = self.rotacao_a_esquerda_dct[self.valor]
-
 
 class Motor:
     def __init__(self):
@@ -127,3 +127,5 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
+
+
